bernds/spring

commit 1150f479b824129b2bc3037fc9fef6a72e359bce

AI: callback change and fixes * add GetLosMapResolution() callback to the Legacy C++ AI Interface (not needed for the the others) * fix two bugs in AAI * fix one bug in NTai

AI/Skirmish/AAI/AAIMap.cpp
@@ -138,8 +138,9 @@ void AAIMap::Init()
 		xSize = xMapSize * SQUARE_SIZE;
 		ySize = yMapSize * SQUARE_SIZE;
 
-		xLOSMapSize = xMapSize / 2;
-		yLOSMapSize = yMapSize / 2;
+		const int losMapRes = cb->GetLosMapResolution();
+		xLOSMapSize = xMapSize / losMapRes;
+		yLOSMapSize = yMapSize / losMapRes;
 
 		xDefMapSize = xMapSize / 4;
 		yDefMapSize = yMapSize / 4;
@@ -2214,7 +2215,7 @@ void AAIMap::UpdateRecon()
 	{
 		for(int x = 0; x < xLOSMapSize; ++x)
 		{
-			if(los_map[x + y * yLOSMapSize])
+			if(los_map[x + y * xLOSMapSize])
 			{
 				scout_map[x + y * xLOSMapSize] = 0;
 				last_updated_map[x + y * xLOSMapSize] = frame;

AI/Skirmish/AAI/AAI.cpp
@@ -444,7 +444,7 @@ void AAI::UnitFinished(int unit)
 
 void AAI::UnitDestroyed(int unit, int attacker)
 {
-	// get unit�s id
+	// get unit's id
 	const UnitDef *def = cb->GetUnitDef(unit);
 
 	// get unit's category and position

AI/Skirmish/NTai/AI/NTai/Core/CCached.cpp
@@ -26,6 +26,7 @@ namespace ntai {
 		
 		//CLOG("Getting LOS pointer");
 		losmap = G->cb->GetLosMap();
+		losmapres = G->cb->GetLosMapResolution();
 		
 		//CLOG("initialising enemy cache elements to zero");
 		for(int i = 0; i< 6000; i++){

AI/Skirmish/NTai/AI/NTai/Core/CCached.h
@@ -15,6 +15,7 @@ namespace ntai {
 		CCached(Global* G);
 
 		const unsigned short* losmap;
+		int losmapres;
 		set<int> enemies; // enemies in LOS
 		//vector<ctri> triangles; // contains all the triangle markers being displayed on map
 

AI/Skirmish/NTai/AI/NTai/Core/Global.cpp
@@ -922,7 +922,8 @@ namespace ntai {
 
 	bool Global::InLOS(float3 pos){
 		const unsigned short* lmap = G->cb->GetLosMap();
-		ushort v = lmap[(int(pos.y/16))*G->cb->GetMapWidth()+(int(pos.x/16))];
+		const int lmapres = G->cb->GetLosMapResolution();
+		ushort v = lmap[(int(pos.y/lmapres))*G->cb->GetMapWidth()+(int(pos.x/lmapres))];
 		return (v >0);
 	}
 

AI/Wrappers/LegacyCpp/AIAICallback.cpp
@@ -708,6 +708,14 @@ const unsigned short* CAIAICallback::GetLosMap() {
 	return losMap;
 }
 
+int CAIAICallback::GetLosMapResolution() {
+
+	int fullSize = GetMapWidth() * GetMapHeight();
+	int losSize = sAICallback->Clb_Map_0ARRAY1SIZE0getLosMap(teamId);
+
+	return fullSize / losSize;
+}
+
 const unsigned short* CAIAICallback::GetRadarMap() {
 
 	static unsigned short* radarMap = NULL;

AI/Wrappers/LegacyCpp/AIAICallback.h
@@ -115,6 +115,7 @@ public:
 	virtual const float* GetSlopeMap();
 
 	virtual const unsigned short* GetLosMap();
+	virtual int GetLosMapResolution();
 	virtual const unsigned short* GetRadarMap();
 	virtual const unsigned short* GetJammerMap();
 	virtual const unsigned char* GetMetalMap();

rts/ExternalAI/AICallback.cpp
@@ -973,6 +973,13 @@ const unsigned short* CAICallback::GetLosMap()
 	return &loshandler->losMap[teamHandler->AllyTeam(team)].front();
 }
 
+int CAICallback::GetLosMapResolution()
+{
+	// as this will never be called (it is implemented in CAIAICallback),
+	// it does not matter what we return here.
+	return -1;
+}
+
 const unsigned short* CAICallback::GetRadarMap()
 {
 	return &radarhandler->radarMaps[teamHandler->AllyTeam(team)].front();

commit 66e22370c2bf0194178257087bcfabe8c3604e4f

Fix a segfault when loading AAI on AltoredDivide
The previous commit 1150f479b824129b2bc3037fc9fef6a72e359bce introduced a
losMapRes variable, but didn't modify all places where this resolution was
hardcoded to 2.  This causes a crash in AAIMap::UpdateEnemyScoutingData()
shortly after loading AAI on AltoredDivide.

AI/Skirmish/AAI/AAIMap.cpp
@@ -19,6 +19,7 @@ int AAIMap::xSize;
 int AAIMap::ySize;
 int AAIMap::xMapSize;
 int AAIMap::yMapSize;
+int AAIMap::losMapRes;
 int AAIMap::xLOSMapSize;
 int AAIMap::yLOSMapSize;
 int AAIMap::xDefMapSize;
@@ -138,7 +139,7 @@ void AAIMap::Init()
 		xSize = xMapSize * SQUARE_SIZE;
 		ySize = yMapSize * SQUARE_SIZE;
 
-		const int losMapRes = cb->GetLosMapResolution();
+		losMapRes = cb->GetLosMapResolution();
 		xLOSMapSize = xMapSize / losMapRes;
 		yLOSMapSize = yMapSize / losMapRes;
 
@@ -2219,7 +2220,7 @@ void AAIMap::UpdateRecon()
 			{
 				scout_map[x + y * xLOSMapSize] = 0;
 				last_updated_map[x + y * xLOSMapSize] = frame;
-				++sector_in_los[(2*x / xSectorSizeMap) + (2*y / ySectorSizeMap) * (xSectors+1)];
+				++sector_in_los[(losMapRes*x / xSectorSizeMap) + (losMapRes*y / ySectorSizeMap) * (xSectors+1)];
 			}
 		}
 	}
@@ -2242,8 +2243,8 @@ void AAIMap::UpdateRecon()
 
 		if(def) // unit is within los
 		{
-			x_pos = (int)pos.x / (2 * SQUARE_SIZE);
-			y_pos = (int)pos.z / (2 * SQUARE_SIZE);
+			x_pos = (int)pos.x / (losMapRes * SQUARE_SIZE);
+			y_pos = (int)pos.z / (losMapRes * SQUARE_SIZE);
 
 			// make sure unit is within the map (e.g. no aircraft that has flown outside of the map)
 			if(x_pos >= 0 && x_pos < xLOSMapSize && y_pos >= 0 && y_pos < yLOSMapSize)
@@ -2254,7 +2255,7 @@ void AAIMap::UpdateRecon()
 				if(cat >= STATIONARY_DEF && cat <= SUBMARINE_ASSAULT)
 				{
 					scout_map[x_pos + y_pos * xLOSMapSize] = def->id;
-					++sector_in_los_with_enemies[(2 * x_pos) / xSectorSizeMap + (xSectors + 1) * ((2 * y_pos) / ySectorSizeMap) ];
+					++sector_in_los_with_enemies[(losMapRes * x_pos) / xSectorSizeMap + (xSectors + 1) * ((losMapRes * y_pos) / ySectorSizeMap) ];
 				}
 
 				if(cat >= GROUND_ASSAULT && cat <= SUBMARINE_ASSAULT)
@@ -2360,9 +2361,9 @@ void AAIMap::UpdateEnemyScoutingData()
 			fill(sector->enemy_stat_combat_power.begin(), sector->enemy_stat_combat_power.end(), 0);
 			fill(sector->enemy_mobile_combat_power.begin(), sector->enemy_mobile_combat_power.end(), 0);
 
-			for(int y = sector->y * ySectorSizeMap/2; y < (sector->y + 1) * ySectorSizeMap/2; ++y)
+			for(int y = sector->y * ySectorSizeMap/losMapRes; y < (sector->y + 1) * ySectorSizeMap/losMapRes; ++y)
 			{
-				for(int x = sector->x * xSectorSizeMap/2; x < (sector->x + 1) * xSectorSizeMap/2; ++x)
+				for(int x = sector->x * xSectorSizeMap/losMapRes; x < (sector->x + 1) * xSectorSizeMap/losMapRes; ++x)
 				{
 					def_id = scout_map[x + y * xLOSMapSize];
 

AI/Skirmish/AAI/AAIMap.h
@@ -175,7 +175,8 @@ public:
 
 	static int xSize, ySize;					// x and y size of the map (unit coordinates)
 	static int xMapSize, yMapSize;				// x and y size of the map (map coordinates)
-	static int xLOSMapSize, yLOSMapSize;		// x and y size of the LOS map (1/2 resolution of map)
+	static int losMapRes;				// resolution of the LOS map
+	static int xLOSMapSize, yLOSMapSize;		// x and y size of the LOS map
 	static int xDefMapSize, yDefMapSize;		// x and y size of the defence maps (1/4 resolution of map)
 	static int xContMapSize, yContMapSize;		// x and y size of the continent maps (1/4 resolution of map)
 	static int xSectors, ySectors;				// number of sectors